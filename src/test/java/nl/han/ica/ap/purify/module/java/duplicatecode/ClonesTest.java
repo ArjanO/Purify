25e402cfbb899be3c36d576a9f3184e1aba71848
ArjanO <arjano@outlook.com>
2013-04-16 10:20:43 +0200

    Added unit test for Clones class.

---
+++ src/test/java/nl/han/ica/ap/purify/module/java/duplicatecode/ClonesTest.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2013 HAN University of Applied Sciences
+ * Arjan Oortgiese
+ * Boyd Hofman
+ * Joëll Portier
+ * Michiel Westerbeek
+ * Tim Waalewijn
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package nl.han.ica.ap.purify.module.java.duplicatecode;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test the storing of detected clones.
+ * 
+ * @author Arjan
+ */
+public class ClonesTest {	
+	private Clones clones;
+	
+	private ParseTree left;
+	private ParseTree right;
+	
+	@Before
+	public void setUp() {
+		clones = new Clones();
+		
+		left = createMock(ParseTree.class);
+		right = createMock(ParseTree.class);
+	}
+	
+	@Test
+	public void addPairToEmptySetTest() {
+		clones.addClonePair(left, right);
+		
+		replay(left);
+		replay(right);
+		
+		assertEquals(1, clones.size());
+		
+		verify(left);
+		verify(right);
+	}
+}
