79600dbf7c1dfe36fab5bbfd4110c636f802a879
ArjanO <arjano@outlook.com>
2013-04-24 11:25:11 +0200

    Added concept node detection for the CFG (control flow graph).

---
+++ src/main/java/nl/han/ica/ap/purify/module/java/extractmethod/StatementVisitor.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2013 HAN University of Applied Sciences
+ * Arjan Oortgiese
+ * Boyd Hofman
+ * Joëll Portier
+ * Michiel Westerbeek
+ * Tim Waalewijn
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package nl.han.ica.ap.purify.module.java.extractmethod;
+
+import nl.han.ica.ap.purify.language.java.JavaBaseVisitor;
+import nl.han.ica.ap.purify.language.java.JavaParser.BlockStatementContext;
+import nl.han.ica.ap.purify.language.java.JavaParser.MethodBodyContext;
+import nl.han.ica.ap.purify.language.java.JavaParser.StatementExpressionContext;
+
+/**
+ * @author Arjan
+ *
+ */
+public class StatementVisitor extends JavaBaseVisitor<Void> {
+	private int number;
+	
+	public StatementVisitor() {
+		number = 0;
+	}
+	
+	@Override
+	public Void visitMethodBody(MethodBodyContext ctx) {
+		number = 1;
+		
+		System.out.println("-----------------------------------");
+		System.out.println(number + " : " + ctx.getText());
+		
+		Void result = super.visitMethodBody(ctx);
+		
+		System.out.println("-----------------------------------");
+		
+		return result;
+	}
+	
+	@Override
+	public Void visitBlockStatement(BlockStatementContext ctx) {
+		number += 1;
+		System.out.println(number + " : " + ctx.getText());
+		
+		return super.visitBlockStatement(ctx);
+	}
+	
+	@Override
+	public Void visitStatementExpression(StatementExpressionContext ctx) {
+		if (ctx.parent.parent instanceof BlockStatementContext) {
+			return null;
+		}
+		
+		number += 1;
+		System.out.println(number + " : " + ctx.getText());
+		
+		return super.visitStatementExpression(ctx);
+	}
+}
