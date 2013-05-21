f309f98bc130343cf6487f9a21c7ac139d6b58b3
ArjanO <arjano@outlook.com>
2013-05-21 13:23:54 +0200

    Added RemoveParameterIssue class.

---
+++ src/main/java/nl/han/ica/ap/purify/module/java/removeparameter/RemoveParameterIssue.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2013 HAN University of Applied Sciences
+ * Arjan Oortgiese
+ * Boyd Hofman
+ * Joëll Portier
+ * Michiel Westerbeek
+ * Tim Waalewijn
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package nl.han.ica.ap.purify.module.java.removeparameter;
+
+import java.util.List;
+
+import nl.han.ica.ap.purify.language.java.JavaParser.FormalParameterDeclsRestContext;
+import nl.han.ica.ap.purify.language.java.JavaParser.MethodDeclarationContext;
+import nl.han.ica.ap.purify.modles.IIssue;
+
+/**
+ * Detected unused parameter.
+ * 
+ * @author Arjan
+ */
+public class RemoveParameterIssue implements IIssue {
+	private MethodDeclarationContext method;
+	private List<FormalParameterDeclsRestContext> parameters;
+	
+	/**
+	 * The parameter in method is not used.
+	 * 
+	 * @param method Method
+	 * @param parameters List with unused parameters.
+	 * @throws IllegalArgumentException If method or parameter is null.
+	 */
+	public RemoveParameterIssue(MethodDeclarationContext method, 
+			List<FormalParameterDeclsRestContext> parameters) {
+		if (method == null || parameters == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		this.method = method;
+		this.parameters = parameters;
+	}
+	
+	/**
+	 * Get the method with the unused parameter.
+	 * 
+	 * @return Method.
+	 */
+	public MethodDeclarationContext getMethod() {
+		return method;
+	}
+	
+	/**
+	 * Get the unused parameters.
+	 * 
+	 * @return Unused parameters.
+	 */
+	public List<FormalParameterDeclsRestContext> getParameters() {
+		return parameters;
+	}
+}
