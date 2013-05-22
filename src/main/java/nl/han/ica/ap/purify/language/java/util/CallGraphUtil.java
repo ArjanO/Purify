386f9ab19fb48ebc82db9fea21f7b0542179ae4e
ArjanO <arjano@outlook.com>
2013-05-22 13:43:30 +0200

    Added CallGraphUtil class.

---
+++ src/main/java/nl/han/ica/ap/purify/language/java/util/CallGraphUtil.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2013 HAN University of Applied Sciences
+ * Arjan Oortgiese
+ * Boyd Hofman
+ * Joëll Portier
+ * Michiel Westerbeek
+ * Tim Waalewijn
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package nl.han.ica.ap.purify.language.java.util;
+
+import nl.han.ica.ap.purify.language.java.JavaParser.MemberDeclContext;
+
+/**
+ * Tools for the call graph.
+ * 
+ * @author Arjan
+ */
+public class CallGraphUtil {
+	private CallGraphUtil() {
+	}
+	
+	/**
+	 * Get call graph method id.
+	 * 
+	 * @param ctx Method
+	 * @return String with method id for the call graph.
+	 */
+	public static String getCallGraphMethodId(MemberDeclContext ctx) {
+		CallGraphMethodIdVisitor visitor = new CallGraphMethodIdVisitor();
+		visitor.visit(ctx);
+		
+		return visitor.getMethodId();
+	}
+}
