b86a87faef78894b2020b929ac2939baa6088296
ArjanO <arjano@outlook.com>
2013-05-26 15:14:19 +0200

    Added HtmlCodeSnippet.

---
+++ src/main/java/nl/han/ica/ap/purify/report/html/HtmlCodeSnippet.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2013 HAN University of Applied Sciences
+ * Arjan Oortgiese
+ * Boyd Hofman
+ * Joëll Portier
+ * Michiel Westerbeek
+ * Tim Waalewijn
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package nl.han.ica.ap.purify.report.html;
+
+import nl.han.ica.ap.purify.report.ICodeSnippet;
+
+/**
+ * HTML code snippet.
+ * 
+ * @author Arjan
+ */
+class HtmlCodeSnippet implements ICodeSnippet {
+	private String code;
+	
+	@Override
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * Get the HTML representation of the code snippet.
+	 * 
+	 * @return HTML representation.
+	 */
+	public String toHtml() {
+		StringBuilder html = new StringBuilder();
+		
+		html.append("<p><pre>\n");
+		html.append(code);
+		html.append("</pre></p>");
+		
+		return html.toString();
+	}
+}
